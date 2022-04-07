e2ede7de7cb4c78d4dc9a08db3c2ef281b3ac315
Phil Stollery <phils@contentmaster.com>
2022-04-07 09:50:20 +0100

    New setup process

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+resource=`az group list --query '[0].name' --output tsv`
+
+az vm create \
+  --resource-group $resource \
+  --name virtualMachine1 \
+  --location westus \
+  --image UbuntuLTS \
+  --admin-username azureuser \
+  --public-ip-sku Standard \
+  --generate-ssh-keys
+
+az vm open-port \
+--port 22 \
+--resource-group $resource \
+--name virtualMachine1
+
+az vm create \
+  --resource-group $resource \
+  --name virtualMachine1 \
+  --location uksouth \
+  --image UbuntuLTS \
+  --admin-username azureuser \
+  --public-ip-sku Standard \
+  --generate-ssh-keys
+
+az vm open-port \
+--port 22 \
+--resource-group $resource \
+--name virtualMachine1
